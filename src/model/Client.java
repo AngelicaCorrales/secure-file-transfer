5eafdfb720207bb3aa31bc4c120bbcd3e8661470
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2023-05-30 23:20:11 -0500

    Client class modifications

--- src/model/Client.java
+++ src/model/Client.java
@@ -10,83 +10,136 @@ import javax.crypto.*;
 import javax.crypto.spec.*;
 
 public class Client {
+    private Socket socket;
+	public static  final int PORT = 12345;
+    public static  final String SERVER_IP = "localhost";
 
-    public static void main(String[] args) {
-    	
-    	
+    private Cipher cipher;
+    private DataInputStream input;
+	private DataOutputStream output;
+
+    public void start(String[] args) {
     	System.setProperty("jdk.crypto.KeyAgreement.legacyKDF", "true");
 
-        if (args.length != 1) {
-            System.out.println("Uso: java Cliente <nombre_archivo>");
-            return;
-        }
+        checkArguments(args);
 
-        String nombreArchivo = args[0];
-        String servidorIP = "localhost";
-        int puerto = 12345;
+        String FILE_NAME = args[0];
 
         try {
-            Socket socket = new Socket(servidorIP, puerto);
-            System.out.println("Connected");
+            connecting();
             
-            DataInputStream entrada = new DataInputStream(socket.getInputStream());
-            DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+            initializeDataStream();
 
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DiffieHellman");
-            keyPairGenerator.initialize(2048);
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            PublicKey clientePublicKey = keyPair.getPublic();
-            byte[] publicKeyBytes = clientePublicKey.getEncoded();
-            salida.writeInt(publicKeyBytes.length);
-            salida.write(publicKeyBytes);
+            //Generated pair keys (public and private)
+            KeyPair keyPair=generateClientKeys();
+            PublicKey clientPublicKey = keyPair.getPublic();
+            PrivateKey clientPrivateKey=keyPair.getPrivate();
 
-            int serverPublicKeyLength = entrada.readInt();
-            byte[] serverPublicKeyBytes = new byte[serverPublicKeyLength];
-            entrada.readFully(serverPublicKeyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("DiffieHellman");
-            X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(serverPublicKeyBytes);
-            PublicKey serverPublicKey = keyFactory.generatePublic(x509KeySpec);
+            //Send key to server
+            sendPublicKeyToServer(clientPublicKey);
 
-            PrivateKey privateKey=keyPair.getPrivate();
-            KeyAgreement keyAgreement = KeyAgreement.getInstance("DiffieHellman");
-            keyAgreement.init(privateKey);
-            keyAgreement.doPhase(serverPublicKey, true);
+            //Diffie Hellman Key exchange
+        	PublicKey serverPublicKey = getServerPublicKey();
+
+            KeyAgreement keyAgreement=negotiatedKeyDiffieHellman(clientPrivateKey,serverPublicKey);
 
             MessageDigest sha = MessageDigest.getInstance("SHA-256");
-            byte[] hashedKeyBytes = sha.digest( keyAgreement.generateSecret());
-            SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKeyBytes, "AES");
+            
+            //Decrypt
+            SecretKeySpec secretKeySpec = keyToDecrypt(keyAgreement,sha);
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            byte[] ivBytes = cipher.getIV();
-            salida.writeInt(ivBytes.length);
-            salida.write(ivBytes);
+           
+            sendIV(cipher,secretKeySpec);
 
-            try (FileInputStream archivoEntrada = new FileInputStream(nombreArchivo)) {
-                byte[] buffer = new byte[64];
-                int bytesRead;
-                while ((bytesRead = archivoEntrada.read(buffer)) != -1) {
-                    byte[] encryptedBytes = cipher.update(buffer, 0, bytesRead);
-                    salida.writeInt(encryptedBytes.length);
-                    salida.write(encryptedBytes);
-                }
-                byte[] encryptedBytes = cipher.doFinal();
-                salida.writeInt(encryptedBytes.length);
-                salida.write(encryptedBytes);
-            }
+            sendingFile(cipher, FILE_NAME);
 
- 
-            byte[] hashBytes = sha.digest(Files.readAllBytes(Paths.get(nombreArchivo)));
-            salida.writeInt(hashBytes.length);
-            salida.write(hashBytes);
+            //Sending hash to server
+            sendingHashToServer(sha,FILE_NAME);
 
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public void getFilePath(String path){
-		
-	}
+
+    public static void checkArguments(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java Cliente <nombre_archivo>");
+            return;
+        }
+    }
+
+    private void connecting() throws IOException {
+        socket = new Socket(SERVER_IP, PORT);
+        System.out.println("Connected");
+   }
+
+    private void initializeDataStream() throws IOException {
+        input = new DataInputStream(socket.getInputStream());
+        output = new DataOutputStream(socket.getOutputStream());
+    }
+
+    private KeyPair generateClientKeys() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DiffieHellman");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+   }
+
+   private void sendPublicKeyToServer(PublicKey clientePublicKey) throws IOException {
+        byte[] publicKeyBytes = clientePublicKey.getEncoded();
+        output.writeInt(publicKeyBytes.length);
+        output.write(publicKeyBytes);
+    }
+   
+
+   private PublicKey getServerPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        int serverPublicKeyLength = input.readInt();
+        byte[] serverPublicKeyBytes = new byte[serverPublicKeyLength];
+        input.readFully(serverPublicKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("DiffieHellman");
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(serverPublicKeyBytes);
+        return keyFactory.generatePublic(x509KeySpec);
+    }
+
+    private KeyAgreement negotiatedKeyDiffieHellman(PrivateKey clientPrivateKey, PublicKey serverPublicKey) throws NoSuchAlgorithmException, InvalidKeyException {
+    	KeyAgreement keyAgreement = KeyAgreement.getInstance("DiffieHellman");
+        keyAgreement.init(clientPrivateKey);
+        keyAgreement.doPhase(serverPublicKey, true);
+        return keyAgreement;
+    }
+
+    private SecretKeySpec keyToDecrypt(KeyAgreement keyAgreement, MessageDigest sha) throws NoSuchAlgorithmException {
+    	byte[] hashedKeyBytes = sha.digest( keyAgreement.generateSecret());
+        SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKeyBytes, "AES");
+        return new SecretKeySpec(hashedKeyBytes, "AES");
+    }
+
+    private void sendIV(Cipher cipher, SecretKeySpec secretKeySpec) throws Exception {
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        byte[] ivBytes = cipher.getIV();
+        output.writeInt(ivBytes.length);
+        output.write(ivBytes);
+    }
+
+    private void sendingFile(Cipher cipher, String file) throws IOException, IllegalBlockSizeException, BadPaddingException {
+    	try (FileInputStream fileSent = new FileInputStream(file)) {
+            byte[] buffer = new byte[64];
+            int bytesRead;
+            while ((bytesRead = fileSent.read(buffer)) != -1) {
+                byte[] encryptedBytes = cipher.update(buffer, 0, bytesRead);
+                output.writeInt(encryptedBytes.length);
+                output.write(encryptedBytes);
+            }
+            byte[] encryptedBytes = cipher.doFinal();
+            output.writeInt(encryptedBytes.length);
+            output.write(encryptedBytes);
+        }
+    }
+
+    private void sendingHashToServer(MessageDigest sha, String file) throws IOException{
+    	byte[] hashBytes = sha.digest(Files.readAllBytes(Paths.get(file)));
+        output.writeInt(hashBytes.length);
+        output.write(hashBytes);
+    }
 }
